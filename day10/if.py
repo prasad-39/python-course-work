0f9f5a6929ed8a77595aa6bcc8f1370da69fa066
Bhavani prasad <gbprasad903@gmail.com>
2026-08-19 09:17:52 +0530

    conditional_statements -if and ifelse

---
+++ day10/if.py
@@ -0,0 +1,17 @@
+#Simple if
+'''sales=int(input('enter the sales: '))
+
+if sales>1000:
+    print('best saller')
+'''
+
+
+'''eli_acc=eval(input('Eligible Account:'))
+ver_sub=eval(input('Meta verified subscription:'))
+if eli_acc and ver_sub:
+    print('verified badge granted') 
+    '''
+
+rain_status=eval(input('enter the rain status :'))
+if rain_status:
+    print('extra Rain charges Applied')
---
+++ day10/ifelse.py
@@ -0,0 +1,13 @@
+'''username=input('enter user name:')
+password=input('enter your password :')
+if username=='prasad' and password=='998877':
+    print('user found ')
+else:
+    print('user not found')
+   ''' 
+products=['bag','pen','pencil','books']
+a=input('enter a product :')
+if a in products:
+    print(f'{a}  is found')
+else:
+    print('item not found')
