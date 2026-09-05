483011e2747d6a09a0f20601392f93a72b6993f6
Bhavani prasad <gbprasad903@gmail.com>
2026-09-05 11:11:54 +0530

    List Operations and Methods

--- day07/list_methods.py
+++ day07/list_methods.py
@@ -1,5 +1,5 @@
 Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
+Type "copyright", "credits" or "license()" for more information. 
 
 >>> # Creating a list
 >>> l = []
